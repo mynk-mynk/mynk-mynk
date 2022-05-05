525b1d9a46b48e06b7196e7ef7b5e25cf524ee89
Romashkina <da_shootka@mail.ru>
2022-05-05 23:57:17 +0200

    init: virtual-keyboard skeleton with webpack, sass, eslint

---
+++ VIRTUAL_KEYBOARD/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/script.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'), 
+    }),
+    new ESLintPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
