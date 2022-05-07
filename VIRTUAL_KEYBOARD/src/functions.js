9dac27c57717dc16271c7d7d969c87648d6ad25a
Romashkina <da_shootka@mail.ru>
2022-05-07 16:58:40 +0200

    feat: add shift and caplock css, add toggle language function

--- VIRTUAL_KEYBOARD/src/functions.js
+++ VIRTUAL_KEYBOARD/src/functions.js
@@ -13,7 +13,7 @@ export function pageStart() {
     let textArea = document.createElement('textarea');
     textArea.name = 'textarea';
     textArea.id = 'textarea';
-    textArea.rows = '15';
+    textArea.rows = '10';
     textArea.autofocus = true;
     page.append(textArea);
 
@@ -23,11 +23,13 @@ export function pageStart() {
     
     let os = document.createElement('p');
     let lang = document.createElement('p');
+    let span = document.createElement('span');
+    span.classList.add('current-lang');
     os.textContent = 'Keyboard for Windows';
     lang.textContent = 'To switch language: Shift + Alt';
-    page.append(os, lang);
+    span.textContent = 'EN';
+    page.append(os, lang, span);
 }
-
 
 
 export function createKeyboard() {
@@ -49,14 +51,14 @@ function createRow(arr) {
 function createBtn(el) {
     let div = document.createElement('div');
     div.classList.add('keyboard-btn');
-    div.textContent = el.nameEn;
+    div.textContent = el.nameEN;
     return div;
 }
 
 
 export function assignBtnSize() {
     const btns = document.querySelectorAll('.keyboard-btn');
-    const long = ['Tab', 'CapsLock', 'Shift', 'Enter', '→', '←']
+    const long = ['Tab', 'Shift', 'Enter', '→', '←']
 
     for (let btn of btns) {
         if (long.includes(btn.textContent)) {
@@ -66,4 +68,84 @@ export function assignBtnSize() {
             btn.classList.add('space');
         }
     }
+}
+
+
+export function assignBtnClasses() {
+    const btns = document.querySelectorAll('.keyboard-btn');
+    const functional = ['Tab', 'Shift', 'Alt', 'Enter', 'Ctrl', 'Fn'];
+
+    for (let btn of btns) {
+        if (functional.includes(btn.textContent)) {
+            btn.classList.add(`${btn.textContent.toLowerCase()}`);
+        
+        } else if (btn.textContent === '⇪') {
+            btn.classList.add('capsLock');
+        
+        } else if (btn.textContent === '⌫') {
+            btn.classList.add('backSpace');
+
+        } else if (btn.textContent === '⊞') {
+            btn.classList.add('windows');
+        
+        } else if (['←', '↑', '↓', '→'].includes(btn.textContent)) {
+            btn.classList.add(`arrow${btn.textContent}`);
+        
+        } else {
+            btn.classList.add('has-value');
+        }
+    }
+}
+
+
+export function toggleBtn(event) {
+    event.target.classList.toggle('highlighted');
+}
+
+export function activateBtn(event) {
+    event.target.classList.add('highlighted');
+}
+
+export function deactivateBtn(event) {
+    event.target.classList.remove('highlighted');
+}
+
+
+export function addValue(btn) {
+    const textArea = document.querySelector('textarea');
+    textArea.value += btn.textContent;
+}
+
+// function respondToKeyboard() {
+//     btn.classList.add('active');
+//     btn.classList.remove('active');
+// }
+
+
+const changeableSymbols = [...symbols.row1, ...symbols.row2, ...symbols.row3, ...symbols.row4];
+
+export function toggleLanguage() {
+    let lang = document.querySelector('.current-lang');
+    const butns = document.querySelectorAll('.has-value');
+
+     if (lang.textContent === 'EN') {
+        butns.forEach(btn => {
+            for (let item of changeableSymbols) {
+                if (btn.textContent === item.nameEN && item.nameRU) {
+                    btn.textContent = item.nameRU;
+                }
+            }
+        });
+        lang.textContent = 'RU';
+     
+    } else if (lang.textContent === 'RU') {
+        butns.forEach(btn => {
+            for (let item of changeableSymbols) {
+                if (btn.textContent === item.nameRU) {
+                    btn.textContent = item.nameEN;
+                }
+            }
+        });
+        lang.textContent = 'EN';
+     }
 }
--- VIRTUAL_KEYBOARD/src/script.js
+++ VIRTUAL_KEYBOARD/src/script.js
@@ -1,9 +1,27 @@
 import './style.scss';
 
-import { pageStart, createKeyboard, assignBtnSize } from './functions';
+import { pageStart, createKeyboard, assignBtnSize, assignBtnClasses, toggleBtn, activateBtn, deactivateBtn, addValue, toggleLanguage } from './functions';
 
 pageStart();
-console.log('1')
 
 createKeyboard();
 assignBtnSize();
+assignBtnClasses()
+
+const capsLock = document.querySelector('.capsLock');
+const shift = document.querySelectorAll('.shift');
+
+capsLock.addEventListener('click', toggleBtn);
+shift.forEach(el => el.addEventListener('mousedown', activateBtn));
+shift.forEach(el => el.addEventListener('mouseup', deactivateBtn));
+
+
+const valueBtns = document.querySelectorAll('.has-value');
+
+valueBtns.forEach(btn => btn.addEventListener('click', (event) => addValue(event.target)));
+
+
+// for testing toggleLanguage
+const space = document.querySelector('.space'); 
+space.addEventListener('click', toggleLanguage);
+// to be deleted
