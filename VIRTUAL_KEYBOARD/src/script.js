22668d9a8428fc80d064d13661a36d0b9f015b3f
Romashkina <da_shootka@mail.ru>
2022-05-08 17:50:34 +0200

    feat: add toggle capitalisation function

--- VIRTUAL_KEYBOARD/src/script.js
+++ VIRTUAL_KEYBOARD/src/script.js
@@ -1,6 +1,220 @@
 import './style.scss';
+import * as symbols from "./symbols";
 
-import { pageStart, createKeyboard, assignBtnSize, assignBtnClasses, toggleBtn, activateBtn, deactivateBtn, addValue, toggleLanguage } from './functions';
+let lang = 'EN';
+let caps = false;
+
+const changeableSymbols = [...symbols.row1, ...symbols.row2, ...symbols.row3, ...symbols.row4];
+
+
+// Functions
+
+function pageStart() {
+    let page = document.createElement('div');
+    page.classList.add('container');
+    document.body.append(page);
+
+    let header = document.createElement('h1');
+    header.textContent = 'Virtual keyboard';
+    page.append(header);
+
+    let textArea = document.createElement('textarea');
+    textArea.name = 'textarea';
+    textArea.id = 'textarea';
+    textArea.rows = '10';
+    textArea.autofocus = true;
+    page.append(textArea);
+
+    let keyboard = document.createElement('div');
+    keyboard.classList.add('keyboard-container');
+    page.append(keyboard);
+
+    let os = document.createElement('p');
+    let switchLang = document.createElement('p');
+    os.textContent = 'Keyboard for Windows';
+    switchLang.textContent = 'To switch language: Shift + Alt';
+    page.append(os, switchLang);
+}
+
+
+function createKeyboard() {
+    for (let item in symbols) {
+        createRow(symbols[item]);
+    }
+}
+
+
+function createRow(arr) {
+    const keyboard = document.querySelector('.keyboard-container');
+    let row = document.createElement('div');
+    row.classList.add('keyboard-row');
+    arr.forEach(item => row.append(createBtn(item)));
+    keyboard.append(row);
+}
+
+
+function createBtn(el) {
+    let div = document.createElement('div');
+    div.classList.add('keyboard-btn');
+    div.textContent = el[`name${lang}`];
+    return div;
+}
+
+
+function assignBtnSize() {
+    const btns = document.querySelectorAll('.keyboard-btn');
+    const long = ['Tab', 'Shift', 'Enter', '→', '←']
+
+    for (let btn of btns) {
+        if (long.includes(btn.textContent)) {
+            btn.classList.add('long');
+        }
+        if (btn.textContent === ' ') {
+            btn.classList.add('space');
+        }
+    }
+}
+
+
+function assignBtnClasses() {
+    const btns = document.querySelectorAll('.keyboard-btn');
+    const functional = ['Tab', 'Shift', 'Alt', 'Enter', 'Ctrl', 'Fn'];
+
+    for (let btn of btns) {
+        if (functional.includes(btn.textContent)) {
+            btn.classList.add(`${btn.textContent.toLowerCase()}`);
+
+        } else if (btn.textContent === '⇪') {
+            btn.classList.add('capsLock');
+
+        } else if (btn.textContent === '⌫') {
+            btn.classList.add('backSpace');
+
+        } else if (btn.textContent === '⊞') {
+            btn.classList.add('windows');
+
+        } else if (['←', '↑', '↓', '→'].includes(btn.textContent)) {
+            btn.classList.add(`arrow${btn.textContent}`);
+
+        } else {
+            btn.classList.add('has-value');
+        }
+    }
+}
+
+
+function toggleBtn(event) {
+    event.target.classList.toggle('highlighted');
+}
+
+function activateBtn(event) {
+    event.target.classList.add('highlighted');
+}
+
+function deactivateBtn(event) {
+    event.target.classList.remove('highlighted');
+}
+
+
+function onBtnClick(btn) {
+    const textArea = document.querySelector('textarea');
+    const enter = document.querySelector('.enter');
+    const backSpace = document.querySelector('.backSpace');
+    const tab = document.querySelector('.tab');
+
+    if (btn === enter) textArea.value += '\n';
+    if (btn === backSpace) textArea.value = textArea.value.slice(0, -1);
+    if (btn === tab) textArea.value = '    ' + textArea.value;
+
+    if (btn.matches('.has-value')) textArea.value += btn.textContent;
+}
+
+
+function toggleLanguage() {
+    const butns = document.querySelectorAll('.has-value');
+
+    if (lang === 'EN') {
+        butns.forEach(btn => {
+            for (let item of changeableSymbols) {
+                if (btn.textContent === item.nameEN) {
+                    btn.textContent = item.nameRU;
+                    break;
+                }
+            }
+        });
+        lang = 'RU';
+
+    } else if (lang === 'RU') {
+        butns.forEach(btn => {
+            for (let item of changeableSymbols) {
+                if (btn.textContent === item.nameRU) {
+                    // item == btn
+                    btn.textContent = item.nameEN;
+                    break;
+                }
+            }
+        });
+        lang = 'EN';
+    }
+}
+
+
+function toggleCaps() {
+    caps = !caps;
+
+    if (caps) {
+        capitalise();
+
+    } else {
+        deCapitalise();
+    }
+}
+
+
+function capitalise() {
+    const butns = document.querySelectorAll('.has-value');
+    butns.forEach(btn => {
+
+        for (let item of changeableSymbols) {
+            const name = item[`name${lang}`] || '';
+
+            if (btn.textContent === name) {
+
+                if (item[`caps${lang}`]) {
+                    btn.textContent = item[`caps${lang}`];
+
+                } else {
+                    btn.textContent = btn.textContent.toUpperCase();
+                }
+            }
+        }
+    });
+}
+
+
+function deCapitalise() {
+    const butns = document.querySelectorAll('.has-value');
+    butns.forEach(btn => {
+
+        for (let item of changeableSymbols) {
+            const name = item[`name${lang}`] || '';
+
+            if (btn.textContent === item[`caps${lang}`] ||
+                btn.textContent === name.toUpperCase()) {
+
+                if (name) {
+                    btn.textContent = name;
+
+                } else {
+                    btn.textContent = btn.textContent.toLowerCase();
+                }
+            }
+        }
+    });
+}
+
+
+// Start
 
 pageStart();
 
@@ -8,20 +222,27 @@ createKeyboard();
 assignBtnSize();
 assignBtnClasses()
 
+
 const capsLock = document.querySelector('.capsLock');
 const shift = document.querySelectorAll('.shift');
 
+
 capsLock.addEventListener('click', toggleBtn);
+capsLock.addEventListener('click', toggleCaps);
+
 shift.forEach(el => el.addEventListener('mousedown', activateBtn));
 shift.forEach(el => el.addEventListener('mouseup', deactivateBtn));
+shift.forEach(el => el.addEventListener('mousedown', capitalise));
+shift.forEach(el => el.addEventListener('mouseup', deCapitalise));
 
-
-const valueBtns = document.querySelectorAll('.has-value');
-
-valueBtns.forEach(btn => btn.addEventListener('click', (event) => addValue(event.target)));
+const keyboard = document.querySelector('.keyboard-container');
+keyboard.addEventListener('click', (event) => onBtnClick(event.target))
 
 
 // for testing toggleLanguage
-const space = document.querySelector('.space'); 
+const space = document.querySelector('.space');
 space.addEventListener('click', toggleLanguage);
-// to be deleted
+// to be deleted (change to Alt + Shift)
+
+
+
--- VIRTUAL_KEYBOARD/src/symbols.js
+++ VIRTUAL_KEYBOARD/src/symbols.js
@@ -1,12 +1,81 @@
-const row1 = [{'nameEN': '`', 'nameRU': 'ё', 'capsEn': '~', 'capsRu': 'Ё'}, {'nameEN': '1', 'capsEn': '!'}, {'nameEN': '2', 'capsEn': '@', 'capsRu': '"'}, {'nameEN': '3', 'capsEn': '#', 'capsRu': '№'}, {'nameEN': '4', 'capsEn': '$', 'capsRu': ';'}, {'nameEN': '5', 'capsEn': '%'}, {'nameEN': '6', 'capsEn': '^', 'capsRu': ':'}, {'nameEN': '7', 'capsEn': '&', 'capsRu': '?'}, {'nameEN': '8', 'capsEn': '*'}, {'nameEN': '9', 'capsEn': '(*)'}, {'nameEN': '0', 'capsEn': ')'}, {'nameEN': '-', 'capsEn': '_'}, {'nameEN': '=', 'capsEn': '+'}, {'nameEN': '⌫'}];
+const row1 = [
+    {'nameEN': '`', 'nameRU': 'ё', 'capsEN': '~'}, 
+    {'nameEN': '1', 'capsEN': '!', 'nameRU': '1', 'capsRU': '!'}, 
+    {'nameEN': '2', 'capsEN': '@', 'nameRU': '2', 'capsRU': '@'}, 
+    {'nameEN': '3', 'capsEN': '#', 'nameRU': '3', 'capsRU': '№'}, 
+    {'nameEN': '4', 'capsEN': '$', 'nameRU': '4', 'capsRU': ';'}, 
+    {'nameEN': '5', 'capsEN': '%', 'nameRU': '5', 'capsRU': '%'}, 
+    {'nameEN': '6', 'capsEN': '^', 'nameRU': '6', 'capsRU': ':'}, 
+    {'nameEN': '7', 'capsEN': '&', 'nameRU': '7', 'capsRU': '?'}, 
+    {'nameEN': '8', 'capsEN': '*', 'nameRU': '8', 'capsRU': '*'}, 
+    {'nameEN': '9', 'capsEN': '(', 'nameRU': '9', 'capsRU': '('}, 
+    {'nameEN': '0', 'capsEN': ')', 'nameRU': '0', 'capsRU': ')'}, 
+    {'nameEN': '-', 'capsEN': '_', 'nameRU': '-', 'capsRU': '_'}, 
+    {'nameEN': '=', 'capsEN': '+', 'nameRU': '=', 'capsRU': '+'}, 
+    {'nameEN': '⌫'}
+];
 
-const row2 = [{'nameEN': 'Tab'}, {'nameEN': 'q', 'nameRU': 'й'}, {'nameEN': 'w', 'nameRU': 'ц'}, {'nameEN': 'e', 'nameRU': 'у'}, {'nameEN': 'r', 'nameRU': 'к'}, {'nameEN': 't', 'nameRU': 'е'}, {'nameEN': 'y', 'nameRU': 'н'}, {'nameEN': 'u', 'nameRU': 'г'}, {'nameEN': 'i', 'nameRU': 'ш'}, {'nameEN': 'o', 'nameRU': 'щ'}, {'nameEN': 'p', 'nameRU': 'з'}, {'nameEN': '[', 'nameRU': 'х'}, {'nameEN': ']', 'nameRU': 'ъ'}, {'nameEN': '\\', 'capsEn': '|'}];
+const row2 = [
+    {'nameEN': 'Tab'}, 
+    {'nameEN': 'q', 'nameRU': 'й'}, 
+    {'nameEN': 'w', 'nameRU': 'ц'}, 
+    {'nameEN': 'e', 'nameRU': 'у'}, 
+    {'nameEN': 'r', 'nameRU': 'к'}, 
+    {'nameEN': 't', 'nameRU': 'е'}, 
+    {'nameEN': 'y', 'nameRU': 'н'}, 
+    {'nameEN': 'u', 'nameRU': 'г'}, 
+    {'nameEN': 'i', 'nameRU': 'ш'}, 
+    {'nameEN': 'o', 'nameRU': 'щ'}, 
+    {'nameEN': 'p', 'nameRU': 'з'}, 
+    {'nameEN': '[', 'nameRU': 'х'}, 
+    {'nameEN': ']', 'nameRU': 'ъ'}, 
+    {'nameEN': '\\', 'capsEN': '|', 'nameRU': '\\', 'capsRU': '|'}
+];
 
-const row3 = [{'nameEN': '⇪'}, {'nameEN': 'a', 'nameRU': 'ф'}, {'nameEN': 's', 'nameRU': 'ы'}, {'nameEN': 'd', 'nameRU': 'в'}, {'nameEN': 'f', 'nameRU': 'а'}, {'nameEN': 'g', 'nameRU': 'п'}, {'nameEN': 'h', 'nameRU': 'р'}, {'nameEN': 'j', 'nameRU': 'о'}, {'nameEN': 'k', 'nameRU': 'л'}, {'nameEN': 'l', 'nameRU': 'д'}, {'nameEN': ';', 'capsEn': ':', 'nameRU': 'ж'}, {'nameEN': '\'', 'capsEn': '"', 'nameRU': 'э'}, {'nameEN': 'Enter'}];
+const row3 = [
+    {'nameEN': '⇪'}, 
+    {'nameEN': 'a', 'nameRU': 'ф'}, 
+    {'nameEN': 's', 'nameRU': 'ы'}, 
+    {'nameEN': 'd', 'nameRU': 'в'}, 
+    {'nameEN': 'f', 'nameRU': 'а'}, 
+    {'nameEN': 'g', 'nameRU': 'п'}, 
+    {'nameEN': 'h', 'nameRU': 'р'}, 
+    {'nameEN': 'j', 'nameRU': 'о'}, 
+    {'nameEN': 'k', 'nameRU': 'л'}, 
+    {'nameEN': 'l', 'nameRU': 'д'}, 
+    {'nameEN': ';', 'capsEN': ':', 'nameRU': 'ж'}, 
+    {'nameEN': '\'', 'capsEN': '"', 'nameRU': 'э'}, 
+    {'nameEN': 'Enter'}
+];
 
-const row4 = [{'nameEN': 'Shift'}, {'nameEN': 'z', 'nameRU': 'я'}, {'nameEN': 'x', 'nameRU': 'ч'}, {'nameEN': 'c', 'nameRU': 'с'}, {'nameEN': 'v', 'nameRU': 'м'}, {'nameEN': 'b', 'nameRU': 'и'}, {'nameEN': 'n', 'nameRU': 'т'}, {'nameEN': 'm', 'nameRU': 'ь'}, {'nameEN': ',', 'capsEn': '<', 'nameRU': 'б'}, {'nameEN': '.', 'capsEn': '>', 'nameRU': 'ю'}, {'nameEN': '/', 'capsEn': '?'}, {'nameEN': '↑'}, {'nameEN': 'Shift'}];
+const row4 = [
+    {'nameEN': 'Shift'}, 
+    {'nameEN': 'z', 'nameRU': 'я'}, 
+    {'nameEN': 'x', 'nameRU': 'ч'},
+    {'nameEN': 'c', 'nameRU': 'с'}, 
+    {'nameEN': 'v', 'nameRU': 'м'}, 
+    {'nameEN': 'b', 'nameRU': 'и'}, 
+    {'nameEN': 'n', 'nameRU': 'т'}, 
+    {'nameEN': 'm', 'nameRU': 'ь'}, 
+    {'nameEN': ',', 'capsEN': '<', 'nameRU': 'б'}, 
+    {'nameEN': '.', 'capsEN': '>', 'nameRU': 'ю'}, 
+    {'nameEN': '/', 'capsEN': '?', 'nameRU': '.', 'capsRU': ','}, 
+    {'nameEN': '↑'}, 
+    {'nameEN': 'Shift'}
+];
 
-const row5 = [{'nameEN': 'Ctrl'}, {'nameEN': 'Fn'}, {'nameEN': '⊞'}, {'nameEN': 'Alt'}, {'nameEN': ' '}, {'nameEN': 'Alt'}, {'nameEN': 'Ctrl'}, {'nameEN': '←'}, {'nameEN': '↓'}, {'nameEN': '→'}];
+const row5 = [
+    {'nameEN': 'Ctrl'}, 
+    {'nameEN': 'Fn'}, 
+    {'nameEN': '⊞'}, 
+    {'nameEN': 'Alt'}, 
+    {'nameEN': ' '}, 
+    {'nameEN': 'Alt'}, 
+    {'nameEN': 'Ctrl'}, 
+    {'nameEN': '←'}, 
+    {'nameEN': '↓'}, 
+    {'nameEN': '→'}
+];
 
 
 export {row1, row2, row3, row4, row5};
